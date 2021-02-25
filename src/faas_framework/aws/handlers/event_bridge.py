5ff2abc408d28ec327ef76e0a9ca796f29895e5c
nadobando <nalbajari1@icloud.com>
2021-02-25 09:36:20 +0200

    fix: detail_class not None

--- src/faas_framework/aws/handlers/event_bridge.py
+++ src/faas_framework/aws/handlers/event_bridge.py
@@ -3,18 +3,17 @@ import abc
 from faas_framework.aws.handlers.native import NativeHandler
 from faas_framework.aws.models.event_bridge import EventBridgeModel
 # from faas_framework.handlers import BaseFunctionHandler
-from faas_framework.handlers import BaseFunctionHandler
 from faas_framework.models.config import BaseModel
 
 
 class EventBridgeHandler(NativeHandler, abc.ABC):
     request_class = EventBridgeModel
-    detail_class: BaseModel
+    detail_class: BaseModel = None
 
     def __serialize_request__(self, request, context):
         if not issubclass(self.request_class, EventBridgeModel):
             raise Exception(f"request must be subclass of {EventBridgeModel.__name__}")
-        self.request: EventBridgeModel = self.request_class.parse_obj(request)
+        self.request = self.request_class.parse_obj(request)
         if self.detail_class:
             self.request.detail = self.detail_class.parse_obj(self.request.detail)
         else:
